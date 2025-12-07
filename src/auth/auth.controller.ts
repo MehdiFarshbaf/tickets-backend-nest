3095ca333f7ee312774ac63236f8fba79e8325c6
MehdiFarshbaf <mehdifarshbaf92@gmai.com>
2025-12-07 22:58:23 +0330

    complete login and register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, HttpStatus, Post, Req, Res} from '@nestjs/common';
+import {AuthService} from "./auth.service";
+import {Public} from "./decorators/public.decorator";
+import {RegisterDto} from "./dto/register.dto";
+import type {Response} from "express";
+import {LoginDto} from "./dto/login.dto";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {
+    }
+
+    @Public()
+    @Post('register')
+    async register(@Body() registerDto: RegisterDto, @Res() res: Response) {
+        const user = await this.authService.register(registerDto);
+        res.status(HttpStatus.CREATED).json({
+            success: true,
+            message: 'User registered successfully',
+            data: user,
+        });
+    }
+
+    @Public()
+    @Post('login')
+    async login(@Body() loginDto: LoginDto, @Res() res: Response) {
+        const loginData = await this.authService.login(loginDto);
+        res.status(HttpStatus.OK).json({
+            success: true,
+            message: 'Login successfully',
+            data: loginData,
+        })
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import {Injectable, OnModuleInit} from '@nestjs/common';
+import {ConflictException, Injectable, NotFoundException, OnModuleInit, UnauthorizedException} from '@nestjs/common';
 import {PrismaService} from "../prisma/prisma.service";
 import {JwtService} from "@nestjs/jwt";
 import * as bcrypt from 'bcrypt';
+import {RegisterDto} from "./dto/register.dto";
+import {LoginDto} from "./dto/login.dto";
 
 @Injectable()
 export class AuthService implements OnModuleInit {
@@ -13,13 +15,59 @@ export class AuthService implements OnModuleInit {
         await this.createFirstAdmin();
     }
 
+    async register(registerDto: RegisterDto) {
+        const existing = await this.prisma.user.findUnique({where: {email: registerDto.email}});
+
+        if (existing) throw new ConflictException('Email already registered');
+
+        const hashed = await bcrypt.hash(registerDto.password, 10);
+
+        return await this.prisma.user.create({
+            data: {
+                email: registerDto.email,
+                password: hashed,
+                name: registerDto.name || null,
+                role: 'USER',
+            },
+            select: {
+                id: true,
+                email: true,
+                name: true,
+                role: true,
+                created_at: true,
+            },
+        });
+    }
+
+    async login(loginDto: LoginDto) {
+        const user = await this.prisma.user.findUnique({where: {email: loginDto.email}});
+        if (!user || !(await bcrypt.compare(loginDto.password, user.password))) throw new NotFoundException('Invalid credentials');
+
+        if (user.is_deleted || !user.is_active) {
+            throw new UnauthorizedException('Account is disabled');
+        }
+
+        const payload = {sub: user.id, email: user.email, role: user.role};
+
+        return {
+            access_token: this.jwtService.sign(payload),
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                role: user.role,
+            },
+        };
+    }
+
+
     private async createFirstAdmin() {
         const adminExists = await this.prisma.user.findFirst({
             where: {role: 'ADMIN'},
         });
 
         if (!adminExists) {
-            const hashed = bcrypt.hashSync('admin123', 12);
+            const hashed = bcrypt.hashSync('admin123', 10);
             await this.prisma.user.create({
                 data: {
                     email: 'admin@ticket.com',
